c4bb643155e341fa3b14e9955e42e93e8d4ad655
Rick Magdaleno <rickmagdaleno@rocketmortgage.com>
2021-10-19 17:41:17 -0700

    Upload of Circle Redux lab, using objects

---
+++ AreaObjects/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaObjects
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+        public double CircleArea => (Radius * 2) * Math.PI;
+        public double CircleCircumference => Radius * Radius * Math.PI;
+
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        //Overrides ToString with radius and circle calculations, while rounding to 2nd decimal place.
+        public override string ToString()
+        {
+            string output = $"Radius: {Math.Round(Radius, 2)}\nCircumference: {Math.Round(CircleCircumference, 2)}\nArea: {Math.Round(CircleArea, 2)}";
+            return output;
+        }
+
+    }
+}
---
+++ AreaObjects/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreaObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> circles = new List<Circle>();
+
+            bool goOn = true;
+            bool circleAgain = true;
+
+            while (circleAgain)
+            {
+                string input = GetInput("Please enter the radius of a circle");
+
+                //Loop to validate radius input. Also asks user if they want to continue to input more circle radius' to add to the radius list.
+                try
+                {
+                    double output = double.Parse(input);
+
+                    if (output >= 0)
+                    {
+                        //ToString Override
+                        Circle c = new Circle(output);
+
+                        //Adds radius input to list, while calculating circle math
+                        circles.Add(c);
+
+                        circleAgain = Continue("Would you like to add another circle?");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number greater than 0\n");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number\n");
+                }
+            }
+                //Once the user decides to stop inputting circles, their inputs and a good-bye message prints        
+                Console.WriteLine();
+                Console.WriteLine($"Before you go, you've entered {circles.Count} circle(s)");
+                Console.WriteLine("Here are the circles you entered:\n");
+                for(int i = 0; i < circles.Count; i++)
+                {
+                    Console.WriteLine($"{i+1}: {circles[i]}" );
+                    Console.WriteLine();
+                }
+            Console.WriteLine("Goodbye!");
+        }
+
+        public static string GetInput(string input)
+        {
+            Console.WriteLine(input);
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        public static bool Continue(string input)
+        {
+            {
+                Console.WriteLine(input);
+                string output = Console.ReadLine().ToLower();
+                if (output == "y")
+                {
+                    return true;
+                }
+                else if (output == "n")
+                {                    
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 'y' or 'n'");
+                    return Continue(input);
+                }
+            }
+        }     
+    }
+}
